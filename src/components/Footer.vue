9092fe742a676cf302f441973377f8f6e5ebcfeb
sansito84 <santiagosito@gmail.com>
2022-08-02 03:59:53 -0300

    changes and missed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
     <footer class="bg-light py-5">
+        <div class="container px-4 px-lg-5"><div class="small text-center text-muted">This site was created with Bootstrap <i class="fa-brands fa-bootstrap" style="font-size:2rem; color: gray"></i><i class="fa-brands fa-vuejs" style="font-size:2rem; color: gray"></i> Vue</div></div>
         <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2022 - Santiago Sito</div></div>
     </footer>     
 </template>
