f64534183d7cdc151d5790383ad25afa347600f3
sansito84 <santiagosito@gmail.com>
2022-08-03 10:06:38 -0300

    modifications

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -5,8 +5,8 @@
             <hr class="divider"/>
             <div class="row">
                 <div class="row text-center">
-                    <div class="col-3 mb-2 mt-4 shadow p-3 rounded-3" v-for="(obj, key) in servicesJSON" :key="key">
-                        <div class="mb-4"><img style="height: 100px" :src="'./'+ obj.img +'.png'" :alt="obj.caption"/></div>
+                    <div class="col mb-2 mt-4 shadow p-3 rounded-3" v-for="(obj, key) in servicesJSON" :key="key">
+                        <div class="mb-4"><img class="services-img zoom" style="height: 100px" :src="'./'+ obj.img +'.png'" :alt="obj.caption"/></div>
                         <h3 class="h4 mb-4">Cliente: {{ obj.header }}</h3>                        
                         <h6 class="mb-2">{{ obj.caption }}</h6>
                         <h6 class="mb-0 font-monospace">{{ obj.description }}</h6>
@@ -23,25 +23,25 @@ export default {
         servicesJSON:[
             {
                 img: 'boca',
-                caption: 'Prototipo "Página web de Boca"',
+                caption: 'Prototipo operativo "Página web de Boca"',
                 header:'Covery',
                 description:'HTML, CSS'
             },
             {
                 img: 'boston',
-                caption: 'Prototipo "Página web de Boston"',
+                caption: 'Prototipo operativo "Página web de Boston"',
                 header:'Covery',
                 description:'HTML, CSS'
             },
             {
                 img: 'libra',
-                caption: 'Prototipo "Página web de Libra"',
+                caption: 'Prototipo operativo "Página web de Libra"',
                 header:'Covery',
                 description:'VUE, Bootstrap'
             },
             {
                 img: 'aeropuertos',
-                caption: 'Prototipo "Página web de Aeropuertos Argentina"',
+                caption: 'Prototipo operativo "Página web de Aeropuertos Argentina"',
                 header:'Covery',
                 description:'HTML, Bootstrap'
             }
