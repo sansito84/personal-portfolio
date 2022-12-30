923c007731e08d2b22ed6e18c0bb2ac28da3bac9
sansito84 <santiagosito@gmail.com>
2022-12-30 14:33:38 -0300

    fixed css bug

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import { createI18n } from 'vue-i18n'
 
 
-import './styles.css'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
