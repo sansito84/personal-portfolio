0373f528c5681d695a0c1fdf4c284eb9b66e3f60
sansito84 <santiagosito@gmail.com>
2024-04-18 11:43:00 -0300

    add yendo

--- src/components/Edu-Component.vue
+++ src/components/Edu-Component.vue
@@ -10,42 +10,44 @@
             <p>{{ course.summary }}</p>
             <p>{{ course.date }}</p>
           </div>
-        </div>        
-        <div v-if="courses.length < 6" :key="`empty-column`" class="col-md-4"></div>
+        </div>
+        <div
+          v-if="courses.length < 6"
+          :key="`empty-column`"
+          class="col-md-4"
+        ></div>
       </div>
     </div>
   </section>
 </template>
 
-<script>
-import coursesData from "../services/courses.json";
 
-export default {
-  data() {
-    return {
-      courses: [],
-    };
-  },
-  mounted() {
-    this.courses = coursesData;
-  }
-};
+<script>
+  import coursesData from "../services/courses.json";
+
+  export default {
+    data() {
+      return {
+        courses: [],
+      };
+    },
+    mounted() {
+      this.courses = coursesData;
+    },
+  };
 </script>
 
 <style>
-
-.course-item {
-  padding: 15px;
-  border-radius: 8px;
-  color: #000;
-  background-color: #ffffff;
-  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-  transition: all 0.3s ease;
-  margin-bottom: 0.3rem;
-}
-.course-item:hover {
+  .course-item {
+    padding: 15px;
+    border-radius: 8px;
+    color: #000;
+    background-color: #ffffff;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease;
+    margin-bottom: 0.3rem;
+  }
+  .course-item:hover {
     transform: translateY(-5px) translateX(-5px);
   }
-
 </style>
-
